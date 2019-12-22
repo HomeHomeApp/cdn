893a7c331b28d75693de26596beecb09323c1501
HomeHomeApp <59135528+HomeHomeApp@users.noreply.github.com>
2019-12-22 19:57:56 +0300

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 $myJSON = '{"name":"test", "level":40, "version":"1.12.22"}';
 
-header('Content-Type: application/json');
+header('Content-Type: application/x-httpd-php');
 
 echo "getVersion(".$myJSON.");";
 
